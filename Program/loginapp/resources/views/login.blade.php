14a69a5a73b20f905f9d07d4ace0218cecaea0fb
muhammad_hamdan_sobirin@teknokrat.ac.id <Priaagung12@gmail.com>
2024-01-23 17:04:39 +0700

    master: menambahkan keamanan content-security-policy

--- Program/loginapp/resources/views/login.blade.php
+++ Program/loginapp/resources/views/login.blade.php
@@ -1,6 +1,8 @@
+<?php
+header("Content-Security-Policy 'self' cdn.tailwindcss.com");
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

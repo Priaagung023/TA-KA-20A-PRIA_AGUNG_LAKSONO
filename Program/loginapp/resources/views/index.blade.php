26c01f9df08a5ff140963a7928cc32b4c67000cd
muhammad_hamdan_sobirin@teknokrat.ac.id <Priaagung12@gmail.com>
2024-01-24 23:27:15 +0700

    master : membuat koneksi Csrf

---
+++ Program/loginapp/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+//Mengirimkan Token Keamanan Ajax Request (Csrf Token)
+session_start();
+if (empty($_SESSION['csrf_token'])) {
+  $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+?>
---
+++ Program/loginapp/resources/views/koneksi.blade.php
@@ -0,0 +1,25 @@
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB1', 'db_dewankomputer');
+
+// Buat Koneksinya
+$db1 = new mysqli(HOST, USER, PASS, DB1);
+<?php
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB1', 'db_ka_21a');
+ 
+// Buat Koneksinya
+$db1 = new mysqli(HOST, USER, PASS, DB1);
+?>
+
+<?php
+//Mengirimkan Token Keamanan Ajax Request (Csrf Token)
+session_start();
+if (empty($_SESSION['csrf_token'])) {
+  $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+?>
